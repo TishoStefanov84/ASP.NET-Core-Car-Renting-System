3e1f5ef7374f30e050022fb12861cdabc614d21a
Tihomir Stefanov <tisho.stefanov@abv.bg>
2022-04-30 09:21:22 +0300

    Implemented adding new cars

---
+++ CarRentingSystem/Controllers/CarsController.cs
@@ -0,0 +1,63 @@
+﻿namespace CarRentingSystem.Controllers
+{
+    using CarRentingSystem.Data;
+    using CarRentingSystem.Data.Models;
+    using CarRentingSystem.Models.Cars;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CarsController : Controller
+    {
+        private readonly CarRentingDbContext data;
+
+        public CarsController(CarRentingDbContext data)
+            => this.data = data;
+
+        public IActionResult Add() => View(new AddCarFormModel
+        {
+            Categoies = this.GetCategories()
+        });
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
+        {
+            if (!this.data.Categories.Any(c => c.Id == car.CategoryId))
+            {
+                this.ModelState.AddModelError(nameof(car.CategoryId), "Category dose not exist.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                car.Categoies = this.GetCategories();
+
+                return View(car);
+            }
+
+            var carData = new Car
+            {
+                Brand = car.Brand,
+                Model = car.Model,
+                Description = car.Description,
+                ImageUrl = car.ImageUrl,
+                Year = car.Year,
+                CategoryId = car.CategoryId
+            };
+
+            this.data.Cars.Add(carData);
+            this.data.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IEnumerable<CarCategoryViewModel> GetCategories()
+            => this.data
+            .Categories
+            .Select(c => new CarCategoryViewModel
+            {
+                Id = c.Id,
+                Name = c.Name
+            })
+            .ToList();
+    }
+}
---
+++ CarRentingSystem/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,38 @@
+﻿namespace CarRentingSystem.Models.Cars
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants;
+
+    public class AddCarFormModel
+    {
+        [Required]
+        [StringLength(CarBrandMaxLength, MinimumLength = CarBrandMinLength)]
+        public string Brand { get; init; }
+
+        [Required]
+        [StringLength(CarModelMaxLength, MinimumLength = CarModelMinLength)]
+        public string Model { get; init; }
+
+        [Required]
+        [StringLength(
+            int.MaxValue, 
+            MinimumLength = CarDescripitonMinLength,
+            ErrorMessage = "The field Description must be a string with a minimum length of {2}.")]
+        public string Description { get; init; }
+
+        [Display(Name = "Image Url")]
+        [Required]
+        [Url]
+        public string ImageUrl { get; init; }
+
+        [Range(CarYearMinValue, CarYearMaxVelue)]
+        public int Year { get; init; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; init; }
+
+        public IEnumerable<CarCategoryViewModel> Categoies { get; set; }
+    }
+}
